f52cc83ece09dc24bdb1416f8ea7227e132e81f0
S BHARATH <141322335+Bharath05ssp@users.noreply.github.com>
2025-03-15 10:48:54 +0530

    Update sortarray.c

--- sortarray.c
+++ sortarray.c
@@ -18,7 +18,7 @@ void sortingArray(int array[] ,int size)
 }
 void printingArray(int array[],int size)
 {
-    for(int i=0;i<size-1;i++)
+    for(int i=0;i<size;i++)
     {
         printf("%d",array[i]);
     }
